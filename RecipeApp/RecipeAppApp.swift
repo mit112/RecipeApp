5cba10b8874969537b2c98cccb4f8a9c014891fc
Mit Sheth <miteduc8@gmail.com>
2024-11-08 18:29:22 -0500

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Mit Sheth on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
